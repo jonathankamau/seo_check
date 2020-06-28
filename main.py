2424c1af1a9852719ef8a441ae84c685c77c8337
jonathankamau <kamaujonathan@gmail.com>
2020-06-28 16:59:50 +0300

    add condition for when matches are found for each keyword

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
+import re
 from urllib import request
-from bs4 import BeautifulSoup
 from urllib.error import HTTPError
 
 site = input("Enter the site you would like to analyze for keywords: ")
 keyword_file = open('keywords.txt', 'r')
 
 def keyword_checker():
-    # result_list = []
+    result_list = []
     try:
-        site_content = request.urlopen(site).read()
+        site_content = request.urlopen(site).read().decode('utf-8')
     except HTTPError as e:
         print(e)
 
-    data = BeautifulSoup(site_content)
-    print('data', data)
-
     for each_line in keyword_file:
-        print('each_line', each_line.lower())
         stripped_line = each_line.lower().strip()
 
-        result_list = data.find_all('p', string=stripped_line)
-        print('result_list', result_list)
+        matches = re.findall(stripped_line, site_content)
+
+        if len(matches) == 0:
+            result = f' \u274C No match was found for {stripped_line}'
+        else:
+            result = f' \u2705 {stripped_line} appears {len(matches)} times'
+
+        print(result)
+        
 
 
 
